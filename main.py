6da9e789fad347bcd3c79c0d3d8d8a7aea3b69f6
Keiseker <juliamine1538@gmail.com>
2023-09-09 16:32:07 +0300

    Prototype version

--- main.py
+++ main.py
@@ -20,10 +20,8 @@ import tts
 # № для функцмй
 import random
 import requests
-from num2t4ru import num2text
-import sqlite3
+from bs4 import BeautifulSoup as b
 import webbrowser
-
 # № для распознавания
 
 
@@ -39,115 +37,88 @@ device = sd.default.device
 samplerate = int(sd.query_devices(device[0], 'input')['default_samplerate'])
 model = vosk.Model('vosk_model_small')
 
-# № для машинного обучения
-# # передача образцов фраз, чтобы найти закономерность
-vectorizer = CountVectorizer()
-vectors = vectorizer.fit_transform(list(words.data_set.keys()))
-
-# # передача образцов фраз для ответа бота
-clf = LogisticRegression()
-clf.fit(vectors, list(words.data_set.values()))
-
-# # освобождение оперативной памяти
-del words.data_set
-
-# № для main
-work = True
-
 
 class VoiceAssistant(QMainWindow, threading.Thread, Ui_MainWindow):
     def __init__(self):
         super(VoiceAssistant, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.thread = None
         self.ui.btn_start.clicked.connect(self.start_thread_assist)
         self.ui.btn_stop.clicked.connect(self.off)
+        self.ui.btn_stop.setEnabled(False)
+
+        # № для main
+        self.work = True
+        # № для машинного обучения
+        # # передача образцов фраз, чтобы найти закономерность
+        self.vectorizer = CountVectorizer()
+        self.vectors = self.vectorizer.fit_transform(
+            list(words.data_set.keys()))
+
+        # # передача образцов фраз для ответа бота
+        self.clf = LogisticRegression()
+        self.clf.fit(self.vectors, list(words.data_set.values()))
+
+        # # освобождение оперативной памяти
+        del words.data_set
 
 # ? КОМАНДЫ ГОЛОСОВОГО ПОМОЩНИКА
     def youtube_search(self):
-        tts.va_speak('Скажите название ролика, который я должна найти')
+        self.Eva('Скажите название ролика, который я должна найти.')
         search = self.listen_command()
         self.user(search)
-        tts.va_speak('Ищу ролик на ютубе')
-        self.Eva('Ищу ролик на ютубе')
+        self.Eva('Ищу ролик на ютубе.')
         webbrowser.open(
             f'https://www.youtube.com/results?search_query={search}')
 
     def web_search(self):
-        tts.va_speak('Повторите, что вы хотите найти')
+        self.Eva('Повторите, что вы хотите найти.')
         search = self.listen_command()
         self.user(search)
-        tts.va_speak('Сейчас')
-        self.Eva('Сейчас')
+        self.Eva('Сейчас.')
         webbrowser.open(
             f'https://www.google.com/search?q={search}&oq={search}'f'81&aqs=chrome..69i57j46i131i433j0l5.2567j0j7&sourceid=chrome&ie=UTF-8')
 
     def get_anekdote(self):
-        connection = sqlite3.connect('anekdot.db')
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM anekdot ORDER BY RANDOM() LIMIT 1")
-        anekdote_row = cursor.fetchone()
-        connection.close()
-        if anekdote_row:
-            anekdote = anekdote_row[1]
-        else:
-            anekdote = "К сожалению, в базе данных пока нет анекдотов"
-        return anekdote
+        r = requests.get('https://anekdotme.ru/anekdoti_aforizmi/')
+        soup = b(r.text, 'html.parser')
+        anekdots = soup.find_all('div', class_='anekdot_text')
+        anekdots = [c.text for c in anekdots]
+        anekdots = [c.replace('\r\n                    ', '')
+                    for c in anekdots]
+        an = ''
+        index = random.randint(0, len(anekdots)-1)
+        for j in range(len(anekdots[index])):
+            if j % 45 == 0:
+                an = anekdots[index][:j]+'\n'+anekdots[index][j:]
+            else:
+                an = anekdots[index]
+            anekdots[index] = an
+        return anekdots[index]
 
     def send_anekdote(self):
         anekdote = self.get_anekdote()
-        tts.va_speak(anekdote)
+        print(type(anekdote))
         self.Eva(anekdote)
-    # # Функция Выбор города для просмотра погоды
 
-    def choose_city(self):
-        tts.va_speak('Назовите город, где хотите узнать погоду')
-        request_result = self.listen_command()
-        self.user(request_result)
-        self.check_weather(request_result)
-
-    # # Функция Проверка погоды в данный момент в данном городе
-
-    def check_weather(self, city):
-        value = 0
-        try:
-            params = {'q': city, 'units': 'metric',
-                      'lang': 'ru', 'appid': 'a7578a6bf6fc5cc5ad71e3efdee74eb8'}
-            response = requests.get(
-                f'https://api.openweathermap.org/data/2.5/weather', params=params)
-            if not response:
-                raise
-            w = response.json()
-            weather = w["weather"][0]['description']
-            temp = round(w['main']['temp'])
-            tts.va_speak("На улице " + weather + num2text(temp)+"градусов")
-            self.Eva("На улице " + weather + num2text(temp)+"градусов")
-        except:
-            value = 1
-            tts.va_speak("Я вас не понял, повторите")
-            self.Eva('Я вас не поняла, повторите')
-        if value == 1:
-            self.choose_city()
     # # Функция Справка, о функционале ассистента
 
     def info(self):
-        tts.va_speak("Привет, я голосовой ассистент Ева! Я могу принять решение за вас, подкинув монетку, посмотреть текущую погоду в вашем городе, рассказать анекдот, найти нужный сайт или видео. Приятно с вами познакомиться!")
-        self.Eva('Привет, я голосовой ассистент Ева!\nЯ могу принять решение за вас, подкинув монетку, посмотреть текущую погоду в вашем городе,\nрассказать анекдот, найти нужный сайт или видео.\nПриятно с вами познакомиться!')
+        self.Eva('Привет, я голосовой ассистент Ева!\nЯ могу принять решение за вас, подкинув монетку,\nрассказать анекдот, найти нужный сайт или видео.\nПриятно с вами познакомиться!')
     # #Функция-заглушка для разговоров
 
     def passive(self):
         ''''''
 
-    # #Функция "Подбрось монетку"
+    # #Функция "Подбрось монетку"()
 
     def flip_coin(self):
         m = random.randint(0, 1)
         if m == 0:
-            tts.va_speak('Выпала решка')
-            self.Eva("Выпала решка")
+            self.Eva("Выпала решка.")
         else:
-            tts.va_speak('Выпал орел')
-            self.Eva("Выпал орел")
+            self.Eva("Выпал орел.")
 #! КОМАНДЫ ОСНОВНЫЕ ДЛЯ РАБОТЫ
     # # Сообщение Евы
 
@@ -157,6 +128,7 @@ class VoiceAssistant(QMainWindow, threading.Thread, Ui_MainWindow):
             item.setTextAlignment(QtCore.Qt.AlignmentFlag.AlignLeft)
             item.setText('  Ева: ' + text)
             self.ui.listWidget.addItem(item)
+            tts.va_speak(text)
 
     # # Сообщение Пользователя
 
@@ -166,6 +138,7 @@ class VoiceAssistant(QMainWindow, threading.Thread, Ui_MainWindow):
             item.setTextAlignment(QtCore.Qt.AlignmentFlag.AlignRight)
             item.setText('  Вы: ' + text)
             self.ui.listWidget.addItem(item)
+
         # # Распознавание команды
 
     def do_this_command(self, request_result, vectorizer, clf):
@@ -174,24 +147,19 @@ class VoiceAssistant(QMainWindow, threading.Thread, Ui_MainWindow):
         try:
             request_result = request_result.replace(list(trg)[0], '')
         except:
-            """"""
+            request_result = request_result
         # ? формирование запроса пользователя
         text_vector = vectorizer.transform([request_result]).toarray()[0]
         answer = clf.predict([text_vector])[0]
 
         func_name = answer.split()[0]
-        print(request_result)
-        print(answer)
-        print(func_name)
 
-        if ((request_result != '') & (answer == 'passive ')):
-            tts.va_speak("Я вас не поняла, повторите")
-            self.Eva('Я вас не поняла, повторите')
+        if ((request_result != '') & (answer == 'passive')):
+            self.Eva('Я вас не поняла, повторите.')
         elif (request_result == '' or request_result == ' '):
             func_name = 'passive'
         else:
             try:
-                tts.va_speak(answer.replace(func_name, ''))
                 self.Eva(answer.replace(func_name, ''))
 
             except:
@@ -213,38 +181,43 @@ class VoiceAssistant(QMainWindow, threading.Thread, Ui_MainWindow):
     # # Остановка ассистента
 
     def off(self):
-        global work
-        work = False
+        self.ui.btn_start.setEnabled(True)
+        self.ui.btn_stop.setEnabled(False)
+        self.work = False
+        if self.thread is not None:  # Если есть запущенный поток
+            self.thread.join()  # Дождитесь завершения потока
+            self.thread = None  # Удалите ссылку на завершенный поток
+        sys.exit()
 
     # # Запуск ассистента
     def main(self):
-        global work
         item = QtWidgets.QListWidgetItem()
         item.setTextAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
         item.setText('\nЧтобы начать работу с ассистентом скажите: Ева')
         self.ui.listWidget.addItem(item)
-        while True:
+        while self.work:
             command = self.listen_command()
             trg = words.TRIGGERS.intersection(command.split())
             if trg:
-                tts.va_speak('Привет')
-                self.Eva('Привет')
-                tts.va_speak('Слушаю вас')
-                self.Eva('Слушаю вас')
-                while work:
+                self.Eva('Привет, слушаю вас.')
+
+                while self.work:
                     command = self.listen_command()
                     self.user(command)
-                    self.do_this_command(command, vectorizer, clf)
+                    self.do_this_command(command, self.vectorizer, self.clf)
             elif 'пока' in command or 'до свидания' in command:
-                tts.va_speak('До свидания')
-                self.Eva('До свидания')
+                self.Eva('До свидания.')
                 break
             else:
                 """"""
 
     def start_thread_assist(self):
-        thread = threading.Thread(target=self.main, args=())
-        thread.start()
+        self.ui.btn_start.setEnabled(False)
+        self.ui.btn_stop.setEnabled(True)
+        # Начать новый поток только в том случае, если нет активного.
+        if self.thread is None or not self.thread.is_alive():
+            self.thread = threading.Thread(target=self.main, args=())
+            self.thread.start()
 
 
 if __name__ == "__main__":
--- tts.py
+++ tts.py
@@ -2,7 +2,6 @@ import torch
 import sounddevice as sd
 import time
 
-
 language = 'ru'
 model_id = 'ru_v3'
 sample_rate = 48000
@@ -18,10 +17,33 @@ model, _ = torch.hub.load(repo_or_dir='snakers4/silero-models',
                           speaker=model_id)
 model.to(device)
 
+ssml_sample = """
+              <speak>
+              <p>
+                <s>Привет, я голосовой ассистент Ева!</s>
+                <s>Я могу принять решение за вас, подкинув монетку, рассказать анекдот,<break time="300ms"/> найти нужный сайт или видео.</s>
+                <s>Приятно с вами познакомиться!</s>
+              </p>
+              </speak>
+              """
 
-# воспроизводим
-def va_speak(what: str):
-    audio = model.apply_tts(text=what+"..",
+# синтез речи
+text = "Привет, я голосовой ассистент Ева! Я могу принять решение за вас, подкинув монетку, рассказать анекдот, найти нужный сайт или видео. Приятно с вами познакомиться!"
+
+
+def va_speak(text):
+    text = text.split()
+    ssml_sample = "<speak><s>"
+    for i in text:
+        if (i[len(i)-1] == '!') or (i[len(i)-1] == '.') or (i[len(i)-1] == '?'):
+            ssml_sample += ' ' + i+'</s>'+'<s>'
+        elif (i[len(i)-1] == ',' or i[len(i)-1] == ':'):
+            ssml_sample += ' ' + i + '<break time="300ms"/>'
+        else:
+            ssml_sample += ' '+i
+    ssml_sample = ssml_sample[:len(ssml_sample)-3]
+    ssml_sample += "</speak>"
+    audio = model.apply_tts(ssml_text=ssml_sample,
                             speaker=speaker,
                             sample_rate=sample_rate,
                             put_accent=put_accent,
